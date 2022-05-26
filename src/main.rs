8429282e6e6c9df099aac7bfa717e201be3159ac
cosmoslx lin <cosmoslx@gmail.com>
2022-05-26 02:48:03 +0000

    add support thread pool

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,38 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::fs;
+use std::thread;
+use std::time::Duration;
+use rust_playground::ThreadPool;
 
 fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
-
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
-    println!("Request: \n{}", String::from_utf8_lossy(&buffer[..]));
+    Read::read(&mut stream, &mut buffer).unwrap();
+    //println!("Request: \n{}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -46,6 +53,6 @@ fn handle_connection(mut stream: TcpStream) {
         contents
     );
 
-    stream.write(response.as_bytes()).unwrap();
+    Write::write(&mut stream, response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
