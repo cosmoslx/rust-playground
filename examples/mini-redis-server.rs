8d6b05dfe03521c59dd2db60d5a75ed02bfa7077
cosmoslx lin <cosmoslx@gmail.com>
2022-06-24 06:58:25 +0000

    implement shared-state mini-redis server

--- examples/mini-redis-server.rs
+++ examples/mini-redis-server.rs
@@ -1,25 +1,63 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         let (socket, _) = listener.accept().await.unwrap();
 
+        println!("Accepted");
+
+        let db = Arc::clone(&db);
         // process is serial
         //process(socket).await;
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
+async fn process(socket: TcpStream, db: Db) {
+    let mut connection = Connection::new(socket);
 
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_owned(), cmd.value().clone());
+                Frame::Simple("OK".to_owned())
+            },
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            },
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+
+}
+
+
+#[allow(dead_code)]
+async fn process_solo_state(socket: TcpStream) {
     let mut db = HashMap::new();
 
     let mut connection = Connection::new(socket);
@@ -46,7 +84,8 @@ async fn process(socket: TcpStream) {
 }
 
 
-async fn _process_unimplemented(socket: TcpStream) {
+#[allow(dead_code)]
+async fn process_unimplemented(socket: TcpStream) {
     let mut connection = Connection::new(socket);
 
     if let Some(frame) = connection.read_frame().await.unwrap() {
